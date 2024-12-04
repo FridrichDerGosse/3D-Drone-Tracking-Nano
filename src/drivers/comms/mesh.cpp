7955a603cb98efd30a198423a03d106d4256f1dd
Nilusink <nilusink@protonmail.com>
2024-12-04 16:42:16 +0100

    mesh fixes

--- src/drivers/comms/mesh.cpp
+++ src/drivers/comms/mesh.cpp
@@ -134,21 +134,29 @@ bool Client::send(payload_t payload, bool renew, uint8_t target)
 // mesh master
 Server::Server(RF24 *radio, RF24Network *network, RF24Mesh *mesh)
 	: radio(radio), network(network), mesh(mesh)
-{
-}
+{}
 
 void Server::init()
 {
+	if (debugging)
+		Serial.println("initializing");
+
 	// set to master
 	mesh->setNodeID(0);
 
 	// set emmision strengh
 	radio->begin();
 	radio->setPALevel(RF24_PA_MAX, 0);
+
+	if (debugging)
+		Serial.println("set power level");
 }
 
 bool Server::start()
 {
+	if (debugging)
+		Serial.println("starting");
+
 	if (!mesh->begin())
 	{
 		// if mesh.begin() returns false for a master node, then radio.begin() returned false.
@@ -157,6 +165,9 @@ bool Server::start()
 
 		return false;
 	}
+	if (debugging)
+		Serial.println("server started");
+
 	return true;
 }
 
@@ -187,15 +198,20 @@ void Server::update()
 
 			if (debugging)
 			{
-				Serial.print(payload.id);
-				Serial.print(", data: ");
+				Serial.print(header.id);
+				Serial.print(", data: \"");
 				Serial.print(payload.data);
-				Serial.print(" from RF24Network address 0");
+				Serial.print("\" from RF24Network address 0");
 				Serial.println(header.from_node, OCT);
 			}
 
 			// append to queue
-			receive_buffer->add_message(payload.data);
+			// if (!message_written)
+			// {
+			// 	message_written = true;
+			// 	strncpy(last_message, payload.data, 64);
+			// }
+			receive_buffer.add_message(payload.data);
 
 			break;
 
@@ -225,7 +241,8 @@ bool Server::send(payload_t payload, uint8_t target)
 
 bool Server::available()
 {
-	return receive_buffer->available();
+	// return message_written;
+	return receive_buffer.available();
 }
 
 bool Server::get_received_message(char *buffer)
@@ -234,7 +251,8 @@ bool Server::get_received_message(char *buffer)
 		return false;
 	
 	// copy buffer content
-	receive_buffer->read(buffer);
+	// strncpy(buffer, last_message, 64);
+	receive_buffer.read(buffer);
 
 	return true;
 }
